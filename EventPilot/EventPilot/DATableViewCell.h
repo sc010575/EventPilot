9def6ebee1b7897da4beb0122e84cc1dd84255f6
suman chatterjee <suman.chatterjee@prometheanworld.com>
2014-05-28 20:35:39 +0100

    First Initial commit

---
+++ EventPilot/EventPilot/DATableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  DATableViewCell.h
+//  EventPilot
+//
+//  Created by Suman Chatterjee on 28/05/2014.
+//  Copyright (c) 2014 Suman Chatterjee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DATableViewCell : UITableViewCell
+
+@end
